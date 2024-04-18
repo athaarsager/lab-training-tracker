88c280a5d80afb06f307225ec8315e87a1ce8f69
Andrew Haarsager <athaarsager@gmail.com>
2024-04-18 15:32:17 -0500

    Create fetchPeople generator for fetching all people in the database

--- src/redux/sagas/person.saga.js
+++ src/redux/sagas/person.saga.js
@@ -1,8 +1,17 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-function* personSaga() {
+function* fetchPeople() {
+    try {
+        const people = yield axios.get("/api/person");
+        yield put({ type: "SET_PEOPLE", payload: people});
+    } catch (error) {
+        console.error("ERROR in fetchPeople generator:", error);
+    }
+}
 
+function* personSaga() {
+    yield takeLatest("FETCH_PEOPLE", fetchPeople);
 }
 
 export default personSaga;
