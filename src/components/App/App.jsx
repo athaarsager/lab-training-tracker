4f7b2cd89d5b84249fd9efbf210a5cd6b21c46aa
Andrew Haarsager <athaarsager@gmail.com>
2024-04-18 16:49:49 -0500

    Delete unnecessary styling sheets. Install MUI

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react';
-import reactLogo from "/Users/andrewhaarsager/app-projects/lab-training-tracker/src/assets/react.svg";
-import viteLogo from '/vite.svg';
-import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Grid container>
+      <Grid item>
+      <Typography variant="h4">Welcome to the Lab Training Tracker for INSERT INSTITUTION HERE</Typography>
+      </Grid>
+    </Grid>
   )
 }
 
