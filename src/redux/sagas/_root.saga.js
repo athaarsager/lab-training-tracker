53c945acc3fd1dd6c03b109d1b2a61f42a79f7d7
Andrew Haarsager <athaarsager@gmail.com>
2024-04-18 16:10:54 -0500

    Setup boilerplate for training saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import personSaga from "./person.saga";
+import trainingSaga from "./training.saga";
 
 export default function* rootSaga() {
     yield all([
         personSaga(),
+        trainingSaga
     ]);
 }
