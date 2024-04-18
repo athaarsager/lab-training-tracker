5be436ac998264f20683b7b8d87102380441652e
Andrew Haarsager <athaarsager@gmail.com>
2024-04-18 15:48:25 -0500

    Create testStatuses reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import people from "./people.reducer";
 import selectedPerson from "./selectedPerson.reducer";
+import testStatuses from "./testStatuses.reducer";
 
 const rootReducer = combineReducers({
     people,
     selectedPerson,
+    testStatuses,
 });
 
 export default rootReducer;
---
+++ src/redux/reducers/testStatuses.reducer.js
@@ -0,0 +1,12 @@
+const testStatusesReducer = (state = [], action) => {
+    switch (action.type) {
+        case "SET_TEST_STATUSES":
+            return action.payload;
+        case "CLEAR_TEST_STATUSES":
+            return state;
+        default:
+            return state;
+    }
+}
+
+export default testStatusesReducer;
