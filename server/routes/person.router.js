a3e5052142d375ecbdde5112d9eda927bddfda56
Andrew Haarsager <athaarsager@gmail.com>
2024-04-18 13:33:26 -0500

    Create route for getting individual person

--- server/routes/person.router.js
+++ server/routes/person.router.js
@@ -19,4 +19,20 @@ router.get("/", async (req, res) => {
     }
 });
 
+// GET individual person for viewing training details
+router.get("/:id", async (req, res) => {
+    try {
+        // store person's id in url and pass as action.payload
+        const personId = req.params.id;
+        const queryText = `
+        SELECT * from "person" WHERE "id" = $1;
+        `;
+        const result = await pool.query(queryText, [personId]);
+        res.send(result.rows);
+    } catch (error) {
+        console.log("ERROR in individual person GET:", error);
+        res.sendStatus(500);
+    }
+});
+
 module.exports = router;
