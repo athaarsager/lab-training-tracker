05cd5f2c4dd7ee19f454861f515684aa63271d66
Andrew Haarsager <athaarsager@gmail.com>
2024-04-18 14:24:09 -0500

    Create route for getting all tests associated with a certain person

--- server/routes/training.router.js
+++ server/routes/training.router.js
@@ -7,8 +7,12 @@ router.get("/:id", async (req, res) => {
     try {
         // req.params.id will be the id of the person clicked on
         const queryText = `
-        
+        SELECT "person_id", "training_id", "title", "short_title", "validation_length", "date_taken" FROM "training"
+        JOIN "person_training" ON "training_id" = "training"."id"
+        WHERE "person_id" = $1;
         `;
+        const result = await pool.query(queryText, [req.params.id]);
+        res.send(result.rows);
     } catch (error) {
         console.log("ERROR in getting all trainings for a specific person:", error);
         res.sendStatus(500);
