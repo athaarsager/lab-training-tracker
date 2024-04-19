5e569eba275096a4697dce4e676396086a2923f9
Andrew Haarsager <athaarsager@gmail.com>
2024-04-18 19:33:02 -0500

    Create todo list

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,18 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 export default function LandingPage() {
+    // TODO: 
+    // display list of people
+    // Allow individual people to be deleted/removed
+    // Add search bar for searching for specific person
+    // Create dialog for adding a new person
+    // Link to resources page
+    // When an individual person is clicked on, take user to detail page for that person
+
     return (
         <Grid container>
             <Grid item>
-            <Typography variant="h4">Welcome to the Lab Training Tracker for INSERT INSTITUTION HERE</Typography>
+                <Typography variant="h4">Welcome to the Lab Training Tracker for INSERT INSTITUTION HERE</Typography>
             </Grid>
         </Grid>
     )
