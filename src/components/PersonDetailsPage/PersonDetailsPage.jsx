d5b9d2a6110bacbd4df382c89ab76b210a3df1e0
Andrew Haarsager <athaarsager@gmail.com>
2024-04-19 15:11:37 -0500

    Create file for PersonDetailsPage

---
+++ src/components/PersonDetailsPage/PersonDetailsPage.jsx
@@ -0,0 +1,14 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+function PersonDetailsPage() {
+    return (
+        <Grid container>
+            <Grid item xs={10}>
+                <Typography variant="h5">Person Details Page</Typography>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default PersonDetailsPage;
