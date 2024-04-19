797979e739343fb709f60817f4d67a0de820b6a9
Andrew Haarsager <athaarsager@gmail.com>
2024-04-19 10:21:53 -0500

    Create default person object, begin setting up dialog

--- src/components/LandingPage/AddPersonDialog.jsx
+++ src/components/LandingPage/AddPersonDialog.jsx
@@ -1,3 +1,5 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -7,9 +9,27 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 function AddPersonDialog({ open, handleClose }) {
+    const dispatch = useDispatch();
+    const [person, setPerson] = useState(
+        {
+            first_name: "",
+            last_name: "",
+            email: "",
+            is_instructor: false
+        }
+    );
+
+    const handleSubmit = (e) => {
+        dispatch({ type: "ADD_PERSON", payload: person});
+        handleClose();
+    }
     return (
-        <>
-        </>
+        <Dialog
+        open={open} 
+        onClose={handleClose}
+        PaperProps={{ component: "form" /* onSubmit: handleSubmit */ }}
+        >
+        </Dialog>
     );
 }
 
