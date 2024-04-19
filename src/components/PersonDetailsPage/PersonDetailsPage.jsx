7761039087158b35e80dac3ada34d9403086197b
Andrew Haarsager <athaarsager@gmail.com>
2024-04-19 15:42:22 -0500

    Write comments describing tasks to be cocmpleted for this branch

--- src/components/PersonDetailsPage/PersonDetailsPage.jsx
+++ src/components/PersonDetailsPage/PersonDetailsPage.jsx
@@ -7,6 +7,10 @@ function PersonDetailsPage() {
     // Link to lab list page
     // On training page, need to add a backend query where when a training is added, a new entry is added to person_training for everyone, default value false
 
+    // Display person's full details
+    // Display all trainings and whether the person has taken them
+    // Allow a person's information to be updated form here
+    
     return (
         <Grid container>
             <Grid item xs={10}>
