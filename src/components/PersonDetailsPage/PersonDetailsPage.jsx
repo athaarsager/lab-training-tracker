1f11197c3ecd3b8ef32a8c0518a4de386564f403
Andrew Haarsager <athaarsager@gmail.com>
2024-04-20 09:28:43 -0500

    Set and grab the selected person from the store on page load

--- src/components/PersonDetailsPage/PersonDetailsPage.jsx
+++ src/components/PersonDetailsPage/PersonDetailsPage.jsx
@@ -1,3 +1,6 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useParams } from "react-router";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Table from "@mui/material/Table";
@@ -10,14 +13,22 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
 function PersonDetailsPage() {
-
+    const dispatch = useDispatch();
+    const personId = useParams().id;
+    const person = useSelector(store => store.selectedPerson);
+    
     // TODO: 
     // Link to lab list page
     // On training page, need to add a backend query where when a training is added, a new entry is added to person_training for everyone, default value false
 
     // Display person's full details
     // Display all trainings and whether the person has taken them
-    // Allow a person's information to be updated form here
+    // Allow trainings to be updated
+    // Allow a person's information to be updated from here
+
+    useEffect(() => {
+        dispatch({ type: "FETCH_SELECTED_PERSON", payload: personId });
+    }, []);
 
     return (
         <Grid container>
