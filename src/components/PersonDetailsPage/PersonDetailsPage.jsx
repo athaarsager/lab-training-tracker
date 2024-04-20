586b199793582770ba02e9910015a6820f7f5672
Andrew Haarsager <athaarsager@gmail.com>
2024-04-20 09:22:02 -0500

    Add imports for creating a table

--- src/components/PersonDetailsPage/PersonDetailsPage.jsx
+++ src/components/PersonDetailsPage/PersonDetailsPage.jsx
@@ -1,5 +1,13 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 function PersonDetailsPage() {
 
@@ -10,11 +18,12 @@ function PersonDetailsPage() {
     // Display person's full details
     // Display all trainings and whether the person has taken them
     // Allow a person's information to be updated form here
-    
+
     return (
         <Grid container>
             <Grid item xs={10}>
                 <Typography variant="h5">Person Details Page</Typography>
+                {/* Make two tables: one for person's info, the other for training info */}
             </Grid>
         </Grid>
     );
