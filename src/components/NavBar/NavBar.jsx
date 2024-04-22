d830a098f3a0a17177e311d7ac9eb2516ee47fb4
Andrew Haarsager <athaarsager@gmail.com>
2024-04-22 16:57:48 -0500

    Adjust placement of Navbar links on desktop

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,9 +2,12 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 function NavBar() {
     return (
-        <Grid container>
-            <Grid sx={{ height: "5vh", backgroundColor: "#0288d1"}} item xs={12}>
-            <Typography variant="h6">Nav</Typography>
+        <Grid sx={{ height: "6vh", backgroundColor: "#0288d1" }} container>
+            <Grid item xs={10}>
+            </Grid>
+            <Grid item xs={2} display={"flex"} justifyContent={"flex-end"} alignItems={"center"}>
+                    <Typography sx={{ mr: "1rem" }} variant="body">Home</Typography>
+                    <Typography sx={{ mr: "1rem" }} variant="body">Trainings</Typography>
             </Grid>
         </Grid>
     );
