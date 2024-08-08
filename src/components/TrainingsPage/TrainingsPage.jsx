5bce65b3087f0cd50f1218d4dd7ab1d89ab96cac
David Vinson <davidvinson018@gmail.com>
2024-08-08 17:04:48 -0500

    selected training reducer updated to rtk

--- src/components/TrainingsPage/TrainingsPage.jsx
+++ src/components/TrainingsPage/TrainingsPage.jsx
@@ -1,117 +1,127 @@
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import TrainingDialog from "./TrainingDialog";
-import Swal from "sweetalert2";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import TrainingDialog from './TrainingDialog';
+import Swal from 'sweetalert2';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { allSelectedTraining } from '../../redux/reducers/selectedTrainingReducer';
+
 function TrainingsPage() {
-    const dispatch = useDispatch();
-    const trainings = useSelector(store => store.trainings);
-    const selectedTraining = useSelector(store => store.selectedTraining);
+  const dispatch = useDispatch();
+  const trainings = useSelector((store) => store.trainings);
+  const selectedTraining = useSelector((store) => store.selectedTraining);
 
-    // Dialog variables
-    const [dialogIsOpen, setDialogIsOpen] = useState(false);
-    const closeDialog = () => setDialogIsOpen(false);
+  // Dialog variables
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  const closeDialog = () => setDialogIsOpen(false);
 
-    const openUpdateTrainingDialog = (e) => {
-        const training = {
-            id: e.target.dataset.id,
-            title: e.target.dataset.title,
-            short_title: e.target.dataset.short_title,
-            validation_length: e.target.dataset.validation_length
-        }
-        dispatch({ type: "SET_SELECTED_TRAINING", payload: training });
-        setDialogIsOpen(true);
-    }
+  const openUpdateTrainingDialog = (e) => {
+    const training = {
+      id: e.target.dataset.id,
+      title: e.target.dataset.title,
+      short_title: e.target.dataset.short_title,
+      validation_length: e.target.dataset.validation_length,
+    };
+    dispatch(allSelectedTraining(training));
+    setDialogIsOpen(true);
+  };
 
-    const deleteTraining = (e) => {
+  const deleteTraining = (e) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      iconColor: '#dc8020',
+      showCancelButton: true,
+      confirmButtonColor: '#1b9f9f',
+      cancelButtonColor: '#9f1b1b',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch({ type: 'REMOVE_TRAINING', payload: e.target.dataset.training_id });
         Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            iconColor: "#dc8020",
-            showCancelButton: true,
-            confirmButtonColor: "#1b9f9f", 
-            cancelButtonColor: "#9f1b1b",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                dispatch({ type: "REMOVE_TRAINING", payload: e.target.dataset.training_id });
-                Swal.fire({
-                    title: "Deleted!",
-                    text: "The training has been deleted.",
-                    icon: "success",
-                    // success.main
-                    iconColor: "#1b9f9f",
-                    // primary.dark
-                    confirmButtonColor: "#0288d1"
-                })
-            }
+          title: 'Deleted!',
+          text: 'The training has been deleted.',
+          icon: 'success',
+          // success.main
+          iconColor: '#1b9f9f',
+          // primary.dark
+          confirmButtonColor: '#0288d1',
         });
-    }
+      }
+    });
+  };
 
-    useEffect(() => {
-        dispatch({ type: "FETCH_TRAININGS" });
-    }, []);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_TRAININGS' });
+  }, [dispatch]);
 
-    return (
-        <Grid container>
-            <Grid item xs={1}></Grid>
-            <Grid item xs={10}>
-                <Box display="flex" flexDirection="column" alignItems="center">
-                    <Typography sx={{ mb: "1rem", mt: "1rem" }} variant="h4">Trainings</Typography>
-                    <Button onClick={(e) => setDialogIsOpen(true)} sx={{ mb: "1rem" }} variant="contained">Add a New Training</Button>
-                </Box>
-                <TableContainer component={Paper} sx={{ mb: "2rem" }}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Full Title</TableCell>
-                                <TableCell>Short Title</TableCell>
-                                <TableCell>Good For</TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {trainings.map(training => (
-                                <TableRow key={training.id}>
-                                    <TableCell>{training.title}</TableCell>
-                                    <TableCell>{training.short_title}</TableCell>
-                                    <TableCell>{training.validation_length + " year(s)"}</TableCell>
-                                    <TableCell>
-                                        <Button
-                                            variant="outlined"
-                                            color="primary"
-                                            data-id={training.id}
-                                            data-title={training.title}
-                                            data-short_title={training.short_title}
-                                            data-validation_length={training.validation_length}
-                                            onClick={(openUpdateTrainingDialog)}
-                                        >
-                                            Edit Training
-                                        </Button>
-                                    </TableCell>
-                                    <TableCell><Button data-training_id={training.id} onClick={deleteTraining} variant="outlined" color="error">Delete Training</Button></TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TrainingDialog open={dialogIsOpen} handleClose={closeDialog} selectedTraining={selectedTraining} />
-            </Grid>
-            <Grid item xs={1}></Grid>
-        </Grid>
-    );
+  return (
+    <Grid container>
+      <Grid item xs={1}></Grid>
+      <Grid item xs={10}>
+        <Box display='flex' flexDirection='column' alignItems='center'>
+          <Typography sx={{ mb: '1rem', mt: '1rem' }} variant='h4'>
+            Trainings
+          </Typography>
+          <Button onClick={() => setDialogIsOpen(true)} sx={{ mb: '1rem' }} variant='contained'>
+            Add a New Training
+          </Button>
+        </Box>
+        <TableContainer component={Paper} sx={{ mb: '2rem' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Full Title</TableCell>
+                <TableCell>Short Title</TableCell>
+                <TableCell>Good For</TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {trainings.map((training) => (
+                <TableRow key={training.id}>
+                  <TableCell>{training.title}</TableCell>
+                  <TableCell>{training.short_title}</TableCell>
+                  <TableCell>{training.validation_length + ' year(s)'}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant='outlined'
+                      color='primary'
+                      data-id={training.id}
+                      data-title={training.title}
+                      data-short_title={training.short_title}
+                      data-validation_length={training.validation_length}
+                      onClick={openUpdateTrainingDialog}
+                    >
+                      Edit Training
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    <Button data-training_id={training.id} onClick={deleteTraining} variant='outlined' color='error'>
+                      Delete Training
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TrainingDialog open={dialogIsOpen} handleClose={closeDialog} selectedTraining={selectedTraining} />
+      </Grid>
+      <Grid item xs={1}></Grid>
+    </Grid>
+  );
 }
 
 export default TrainingsPage;
--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,16 +2,16 @@
 import { combineReducers } from 'redux';
 import peopleReducer from './people.reducer';
 import selectedPersonReducer from './selectedPerson.reducer';
-import trainingStatuses from './trainingStatuses.reducer';
+import trainingStatusReducer from './trainingStatuses.reducer';
 import trainingsReducer from './trainings.reducer';
-import selectedTraining from './selectedTrainingReducer';
+import selectedTrainingReducer from './selectedTrainingReducer';
 
 const rootReducer = combineReducers({
   people: peopleReducer,
   selectedPerson: selectedPersonReducer,
-  trainingStatuses,
+  trainingStatuses: trainingStatusReducer,
   trainings: trainingsReducer,
-  selectedTraining,
+  selectedTraining: selectedTrainingReducer,
 });
 
 export default rootReducer;
